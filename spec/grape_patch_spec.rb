77a7031fa27207b3b897bc940d57ba07bcb391fd
piasekhgw <wojciech.piasecki@netguru.pl>
2015-02-11 08:17:16 +0100

    simple test case

---
+++ spec/grape_patch_spec.rb
@@ -0,0 +1,21 @@
+require 'grape'
+require 'simple_grape_params/validator'
+require 'simple_grape_params/grape_patch'
+
+class TestValidator < SimpleGrapeParams::Validator
+  params do
+    'test'
+  end
+end
+
+describe Grape::API do
+  describe '.validate_params_with' do
+    before  { allow(described_class).to receive(:params) }
+    it "invokes 'params' method with given block" do
+      expect(described_class).to receive(:params) do |&block|
+        expect(block.call).to eq('test')
+      end
+      described_class.validate_params_with(TestValidator)
+    end
+  end
+end
